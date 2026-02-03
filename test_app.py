cc11f8feaeeb388b529343ae9a7d594cef5983d9
abubakarsaddique22 <abubakarsaddique3434@gmail.com>
2026-02-03 11:52:06 +0500

    changing in test_app file

--- test_app.py
+++ test_app.py
@@ -1,18 +1,19 @@
+from unittest.mock import patch
 import requests
-import time
 
-app_url = "http://localhost:8000"
+APP_URL = "http://localhost:8000"
 
-# get the status code
+
 def get_app_status(url):
     response = requests.get(url)
-    status_code = response.status_code
-    return status_code
+    return response.status_code
 
-# test for the app home page loading
-def test_app_loading():
-    # wait for the app to load
-    time.sleep(60)
-    status_code = get_app_status(app_url)
+
+@patch("requests.get")
+def test_app_loading(mock_get):
+    # mock successful response
+    mock_get.return_value.status_code = 200
+
+    status_code = get_app_status(APP_URL)
+
     assert status_code == 200, "Unable to load Streamlit App"
-    print("Streamlit App Loaded Successfully")
